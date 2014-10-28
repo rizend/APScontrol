9cb062f4930a2ca67f3216ee107362685d685b52
Roy Veatch <lancerroy@gmail.com>
2014-10-28 00:50:21 -0400

    fized it

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ var apt = require('node-apt-get');
 apt.options['assume-yes'] = true;
 var logger=require("./logging.js");
 var tls = require("tls");
+var name="ahsrm321-lx01"
 logger.setStream({write:function(d) {console.log(""+d)}});
+var c = tls.connect({
+		ca: [ fs.readFileSync("./certs/cert.pem") ],
+		host:"192.168.1.5",
+		port:4443 },
+		function() {
+	console.log("connected!");
+	c.write("0:"+name);
+});
+c.setEncoding('utf8');
--- server.js
+++ server.js
@@ -4,6 +4,41 @@ var options = {
   key: fs.readFileSync("./certs/key.pem"),
   cert: fs.readFileSync("./certs/cert.pem")
 };
+var district={};
+district.ahs={};
+district.oms={};
 var server = tls.createServer(options, function(c) {
-	
+	var state=0;
+	var id={};
+	c.setEncoding('utf8');
+	c.on("data". function(d) {
+		switch(state) {
+		case 0:
+			if(d.substr(0,2)==="0:") {
+				if((d.substr(2,3)==="ahs") || (d.substr(2,3)==="oms") &&
+						d.substr(5,2)==="rm" &&
+						parseInt(d.substr(7,3))>99 &&
+						d.substr(10,3)==="-lx" &&
+						parseInt(d.substr(13,2))>0) {
+					id.full=d.substr(2,13);
+					id.school=id.full.substr(2,3);
+					id.room=parseInt(id.full.substr(7,3));
+					id.number=parseInt(id.full.substr(13,2));
+					if(typeof(district["rm"+id.room])==="undefined") {
+						district["rm"+id.room]={};
+					}
+					district[id.school]["rm"+id.room]["lx"+id.number]={id:id, c:c, loggedin:undefined};
+				}
+				state=1;
+			} else {
+				c.end();
+			}
+			break;
+		case 1:
+			
+		default:
+			throw "error:? invalid data";
+		}
+	});
 });
+server.listen(4443);
