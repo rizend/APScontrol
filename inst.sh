c317dfa2b54427b265b409a6e11a1af880153b71
root <root@ahsrm321-lx01.interlab.int>
2014-10-28 12:24:26 -0400

    fized inst.sh

--- inst.sh
+++ inst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -y install nodejs npm
 chmod +x /RV/APScontrol/*.sh
-cp /RV/inst/APScontrol/20-lubuntu.conf /etc/lightdm/lightdm.conf.d/
-cd /RV/APSconstrol/
+cp /RV/APScontrol/20-lubuntu.conf /etc/lightdm/lightdm.conf.d/
+cd /RV/APScontrol/
 npm install
